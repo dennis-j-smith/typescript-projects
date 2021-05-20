934c0f4dd8b8b4a0e5b0db96865c218a4a7ce2de
Dennis Smith <dennis.j.smith95@gmail.com>
2021-05-19 20:29:14 -0400

    updates

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,12 +1,68 @@
-const names: Array<string> = ['Dennis', 'Max']
-//names[0].split(' ');
+// const names: Array<string> = ['Dennis', 'Max']
+// //names[0].split(' ');
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is done');
-    }, 2000);
-})
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('This is done');
+//     }, 2000);
+// })
 
-promise.then(data => {
-    data.split(' ');
-})
+// promise.then(data => {
+//     data.split(' ');
+// })
+
+// type constraints
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({name: 'Max'}, {age: 30});
+console.log(mergedObj.age)
+
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+    let descriptionText = 'Got no value';
+    if (element.length > 0) {
+        descriptionText = 'Got 1 element.';
+    } else if (element.length > 0) {
+        descriptionText = 'Got ' + element.length + ' elements.';
+    }
+    return [element, descriptionText];
+}
+
+console.log(countAndDescribe('hello there'));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+    return 'Value: ' + obj[key];
+}
+
+console.log(extractAndConvert({name: 'Dennis', phone: '410-555-5555'}, 'name'))
+
+
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+
+    addItem(item: T) {
+        this.data.push(item);
+    }
+
+    removeItem(item: T) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+
+const numberStorage = new DataStorage<number>();
+
+// const objectStorage = new DataStorage<object>();
+// objectStorage.addItem({name: 'Max'})
+// objectStorage.addItem({name: 'Manu'})
